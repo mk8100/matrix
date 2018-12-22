41e3dc67ded161b60cbad831d3bccea6dbc46619
Kapitan Nemo <mk8100@yahoo.com>
2018-12-22 20:54:17 +0100

    Display printing method - initial version

--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,19 @@ pub struct Matrix<T>
         data: Vec<T>,
 }
 
-impl <T> Matrix<T> where T:Mul+Add {
+pub trait MatrixNum<T>: Mul<T> + Add<T> + Rem<T> + Display {}
+impl<T> MatrixNum<T> for T where T: Mul + Add + Rem + Display {}
+
+impl <T> Matrix<T> where T:MatrixNum<T> {
+//impl <T> Matrix<T> where T:Mul+Add+Rem+Display {
 
 /// funtion for creating a new Matrix structure
 /// 
 /// # Arguments
 /// 
-/// * `m:u32` -- rows number
-/// * `n:u32` -- columns number 
+/// * `m:u32` - rows number
+/// * `n:u32` - columns number 
+/// * `v:Vec<T>` - vector of data (numbers) that will be moved into a Matrix struct
 /// 
 /// # Examples
 /// ```
@@ -48,6 +53,38 @@ impl <T> Matrix<T> where T:Mul+Add {
     pub fn data(&self) -> &Vec<T> {
         &self.data
     }
+}
+
+use std::fmt::Display;
+use std::ops::Rem;
+use std::fmt;
+
+impl <T> Display for Matrix<T> where T:Mul+Add+Rem+Display{
+    /// a fmt function for Display Trait implementation
+    ///  
+    /// # Examples
+    /// 
+    /// ```
+    /// let m = matrix::Matrix::new(2,3,vec![1,1,1,2,2,2]);
+    /// println!("{}", m);
+    /// ```
+     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let c:u32 = self.ncols();
+        let cl = |(idx,el):(usize,&T)| {if idx as u32 % c == 0 {el.to_string() + " |\n"}else{el.to_string()+", "}}; 
+        let w:String = self.data.iter()
+                        .enumerate()
+                        .map( cl )
+                        .collect::<Vec<String>>()
+                        .concat();
+        write!(f, "\n{}", w)
+    }
+}
+
+
+#[test]
+fn display_test(){
+    let m = Matrix::new(2,3,vec![1,2,3,4,5,6]);
+    println!("Matrix = {}",m)
 }
 
 /// A function multipliing two numbers
